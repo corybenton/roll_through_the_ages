9f3ebecbd834051524c159cad4c8eea865d34aaf
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-07-18 18:44:17 -0400

    Created instance gamestate, having trouble rendering information to game.handlebars

---
+++ controllers/game-routes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const { GameState, Monuments, Developments, Goods, User } = require('../models');
+
+
+router.post('/game', async (req, res) => {
+  console.log('New game request received:', req.body);
+  try {
+    console.log('User ID:', req.session.user_id);
+    const userId = req.session.user_id;
+    const newGameState = await GameState.create({ player: userId });
+    console.log('gameroute post gamestate: ', newGameState);
+    res.redirect(`/game/${newGameState.id}`);
+    //res.redirect('/game');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.get('/game/:id', async (req, res) => {
+//router.get('/game', async (req, res) => {
+  try {
+    const gameId = req.params.id;
+
+    const GameData = await GameState.findByPk(gameId, {
+      include: [
+        { model: Monuments },
+        { model: Developments },
+        { model: Goods },
+        { model: User },
+      ],
+    });
+
+    if (!GameData) {
+      return res.status(404).json({ error: 'Game state not found' });
+    }
+
+    // Render the 'game' handlebars template and pass the fetched data
+    res.render('game', { game: GameData });
+    //res.render('game');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./home-routes.js');
-const { login, signup, logout } = require('./userController');
+const { login, signup, logout, newGame } = require('./userController');
+
+const gameRoutes = require('./game-routes.js');
 
 router.use('/', homeRoutes);
 
@@ -10,5 +12,10 @@ router.post('/login', login);
 router.post('/signup', signup);
 
 router.post('/logout', logout);
+
+router.post('/game', newGame);
+
+router.use('/game', gameRoutes);
+
 
 module.exports = router;
