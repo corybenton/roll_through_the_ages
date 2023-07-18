720265ff69b7fc5ad661b62d2add3760f40e6f01
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-07-17 20:09:11 -0400

    Revert "changed login fetch url to /login. should be able to login now"
    This reverts commit 858869a8785089f0405560bdbced2847ceef8b1e.

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    const response = await fetch('/login', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
