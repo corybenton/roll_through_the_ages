61ba8acc58ffd6229ae44736ac883be52f456a91
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-07-21 22:48:00 -0400

    Player two adds to game on Join button click  and rendered in game lobby. gameboard is created and added to game, If i'm not mistaken

--- public/js/newGame.js
+++ public/js/newGame.js
@@ -26,3 +26,30 @@ document.querySelector('#newGameBtn').addEventListener('click', async () => {
   }
 });
 
+
+document.querySelector('#joinBtn').addEventListener('click', async () => {
+  console.log('Button clicked!');
+  try {
+    const response = await fetch('/join', {
+      method: 'POST',
+      body: JSON.stringify({ player: 2}),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    console.log('Response:', response);
+
+    if (response.ok) {
+      const responseData = await response.json();
+      //console.log('gamestate id newgame.js:', responseData.gameStateId);
+      //window.location.href = `/game/${responseData.gameStateId}`;
+
+      //Sending get request to game/id
+      window.location.href = `/game/${responseData.newgame}`; // Can't get either of these to direct to the game.handlebars... hmmmmm
+    } else {
+      alert('Failed to Join game.');
+    }
+  } catch (err) {
+    console.error(err);
+    alert('An error occurred.');
+  }
+});
