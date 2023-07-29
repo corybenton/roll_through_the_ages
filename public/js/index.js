798acba97ba930a0ee6f56401ee2164b224547f0
Cory Benton <corybenton@gmail.com>
2023-07-29 19:28:53 -0400

    erase other one

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 const allPlayers = document.querySelectorAll('.player');
 const player = [];
 
-player[0] = allPlayers[0].getAttribute('id');
-player[1] = allPlayers[1].getAttribute('id');
-
-// const newTurn = new Turn;
-
 const startTurn = (e) => {
   e.preventDefault();
   player[0] = allPlayers[0].getAttribute('id');
