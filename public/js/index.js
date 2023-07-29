94cbb53e34aa901d519bd2e4fcb62f74ca0c8f1a
Cory Benton <corybenton@gmail.com>
2023-07-29 18:55:48 -0400

    yet even more tweaks

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,14 @@
 const allPlayers = document.querySelectorAll('.player');
 const player = [];
 
-//player[0] = allPlayers[0].getAttribute('id');
-//player[1] = allPlayers[1].getAttribute('id');
-
+player[0] = allPlayers[0].getAttribute('id');
+if (player[1]) {
+  player[1] = allPlayers[1].getAttribute('id');
+}
 // const newTurn = new Turn;
 
 const startTurn = (e) => {
   e.preventDefault();
-  player[0] = allPlayers[0].getAttribute('id');
-  //player[1] = allPlayers[1].getAttribute('id');
   newTurn.startRoll(player);
 };
 
